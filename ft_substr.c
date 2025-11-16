812bb9cd1ed0638c4f338a681a3d1c37e3235695
Agastya Kumar <agastya-kumar@Agastyas-MacBook-Pro.local>
2025-11-16 21:50:50 +0100

    Final Push

--- ft_substr.c
+++ ft_substr.c
@@ -2,28 +2,39 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t	s_len;
-	size_t	sub_len;
-	char	*str;
+	size_t  s_len;
+	size_t  sub_len;
+	size_t  i;
+	char    *str;
 
 	if (!s)
 		return (NULL);
+
 	s_len = ft_strlen(s);
 	if (start >= s_len)
 	{
 		str = malloc(1);
-		if (str)
-			str[0] = '\0';
+		if (!str)
+			return (NULL);
+		str[0] = '\0';
 		return (str);
 	}
+
 	sub_len = s_len - start;
 	if (sub_len > len)
 		sub_len = len;
+
 	str = malloc(sub_len + 1);
 	if (!str)
 		return (NULL);
-	for (size_t i = 0; i < sub_len; i++)
+
+	i = 0;
+	while (i < sub_len)
+	{
 		str[i] = s[start + i];
-	str[sub_len] = '\0';
+		i++;
+	}
+
+	str[i] = '\0';
 	return (str);
 }
